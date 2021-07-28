75a5216ce88ddb23dd1d93742aa4993551f805fd
Administrator <1303804462@qq.com>
2021-07-28 20:14:47 +0800

    mine_python_7.28

--- other_projects/write_excel.py
+++ other_projects/write_excel.py
@@ -13,13 +13,25 @@ import xlwt
 import xlrd
 from xlutils.copy import copy
 
-oldWb = xlrd.open_workbook(r"D:\python项目\other_projects\five.xls",'w+b')
+oldWb = xlrd.open_workbook(r"D:\python项目\other_projects\first.xls",'w+b')
 #先打开已存在的表
 newWb = copy(oldWb)
 #复制
 newWs = newWb.get_sheet(1)
 #取sheet表
-newWs.write(3, 5, "pass")
-#写入 2行4列写入pass
-newWb.save(r"D:\python项目\other_projects\five.xls")
+newWs.write(2,6, label = 'diyige')
+#下标从零开始，写入第六列第二行写入内容
+newWb.save("D:\python项目\other_projects\end.xls")
 #保存至result路径
+
+# # 创建一个workbook 设置编码
+# workbook = xlwt.Workbook(encoding = 'utf-8')
+# # 创建一个worksheet
+# worksheet = workbook.add_sheet('My Worksheet')
+#
+# # 写入excel
+# # 参数对应 行, 列, 值
+# worksheet.write(1,1, label = 'this is test')
+#
+# # 保存
+# workbook.save('Excel_test2.xls')
